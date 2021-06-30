5aa79266fc520a281cef34f9cead961dc16d383c
daryllast <daryl.last621@gmail.com>
2021-06-29 22:12:41 -0400

    checking in

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const yahooStockPrices = require('yahoo-stock-prices')
+const {Portfolio, Wallet} = require('../lib/models');
+
+// - GET http://localhost:3000/api/v1/search/:symbol
+//     - GET http://localhost:3000/api/v1/portfolio
+//     - POST http://localhost:3000/api/v1/portfolio  {stockSymbol: "AAPL", quantity: 100, price: 50}
+
+//     - DELETE http://localhost:3000/api/v1/portfolio/45
+
+
+router.get('/search/:symbol', async function(req, res, next) {
+    console.log('the req.params are', req.params)
+    const data = await yahooStockPrices.getCurrentData(req.params.symbol);
+    res.json(data)
+});
+
+router.get('/portfolio', async function(req, res, next) {
+    let items = await Portfolio.findAll({});
+    res.json(items);
+});
+
+router.post('/portfolio', function(req, res, next) {
+    res.json({createRowInTheTable: true})
+});
+
+router.delete('/portfolio/:id', function(req, res, next) {
+    res.json({deleteRowInTheTable: true})
+});
+
+/* GET home page. */
+router.get('/test', function(req, res, next) {
+    res.json({api: true})
+});
+
+module.exports = router;
